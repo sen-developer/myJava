77f6bdd388dcf7f14e8c30ac93d59a7030c5bbc7
Sen <senikvardanian@gmail.com>
2022-10-25 09:49:24 +0300

    First git commit

---
+++ MFP/src/com/lesson/sen/Main.java
@@ -0,0 +1,33 @@
+// jdk java development kit parunakuma compilatur ev jvm
+//jre  java runtime invireoment = menak run e anum cod@ byte code
+// byte code - kod@ stanum enq kompilaciaic heto
+
+package com.lesson.sen;
+
+public class Main {
+    public static void main(String[] args) {
+        // primitive types - parz tver
+        byte b = 5; //byte pahum e 0-255 tiv sksac -128ic minchev 127 , 1 byte 8 bit
+        short b2 = 10; // 16 bit 2 bytes range -32,768 32,767
+        int b3 = 65; // size 32 bite 4 bytes
+        long b4 = 8568; // 64 bite 8 bytes
+        char s = 6535; // cuyc a talis arjeq@ text dzevov stjeq 0- 65535 2 bytes
+         // loxacox tver (amboxj) = primitive
+
+        float b5 = 52554.6f; // amboxjakan tver 4 bytes
+        double b6 = 4654651.8; // amboxj tver mec 8bytes zangvats
+
+        //primitive type logical boolean
+        boolean ko = true;//true or false
+
+        // bard tip
+        String str = "hello wee";// toxayin bard tip
+
+
+
+
+        System.out.println(s);
+
+
+    }
+}
---
+++ MFP/src/com/lesson/sen/Main2.java
@@ -0,0 +1,25 @@
+package com.lesson.sen;
+
+public class Main2 {
+    public static void main(String[] args) {
+        byte b;
+        int u = 150;
+        b = (byte) u; //down casting mec tipic qcum enq poqr tipi mej
+        float u8 = 55.8f;
+        u = (int)u8; // hanum rada anum . ic heto inch ka;
+        System.out.println(u);
+
+
+        int i = 5;
+
+
+
+        int sum = i++ + ++i +  i++ + i++ + --i + i--;
+        //        5       7    7     8       8    8
+        // i = 7
+       // sum = 43
+
+        System.out.println("the sum is = "+sum+", i is = "+i);
+
+    }
+}
